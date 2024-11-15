ca698f71afef022d79b9343fba457cf51413655c
ofcljaved <javedofficialme@gmail.com>
2024-11-15 23:48:38 +0530

    init crud dapp

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -2,69 +2,55 @@
 
 use anchor_lang::prelude::*;
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 #[program]
 pub mod crud {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCrud>) -> Result<()> {
-    Ok(())
-  }
+    pub fn create_journal_entry(
+        ctx: Context<CreateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud.count = ctx.accounts.crud.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud.count = ctx.accounts.crud.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCrud>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.crud.count = value.clone();
-    Ok(())
-  }
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.message = message;
+        
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrud<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(title:String)]
+pub struct CreateEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + Crud::INIT_SPACE,
-  payer = payer
-  )]
-  pub crud: Account<'info, Crud>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCrud<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    #[account(
+        init,
+        space = ANCHOR_DISCRIMINATOR_SIZE + JournalEntryState::INIT_SPACE,
+        payer = owner,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub crud: Account<'info, Crud>,
+    pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub crud: Account<'info, Crud>,
-}
-
-#[account]
+#[account()]
 #[derive(InitSpace)]
-pub struct Crud {
-  count: u8,
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+
+    #[max_len(50)]
+    pub title: String,
+
+    #[max_len(280)]
+    pub message: String,
 }
